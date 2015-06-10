00a438a14280ab94957b9479ca0ed497ef56613e
Evgeniy Davydov <jekamozg@mail.ru>
2015-06-10 11:01:08 +0600

    Bug fix - Add route params

--- DependencyInjection/ITEFiltrationExtension.php
+++ DependencyInjection/ITEFiltrationExtension.php
@@ -22,6 +22,7 @@ class ITEFiltrationExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
         $loader->load('form_types.yml');
