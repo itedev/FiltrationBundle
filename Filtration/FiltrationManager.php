8510ed4de0a98781469c3c9d57eb1dcad2845bfd
Dmitry <kislicynda@gmail.com>
2015-06-29 17:27:09 +0600

    fix bug in filtration manager

--- Filtration/FiltrationManager.php
+++ Filtration/FiltrationManager.php
@@ -241,7 +241,7 @@ class FiltrationManager implements FiltrationInterface
             throw new \LogicException('Filter form should have an option "filter_form" set to true.');
         }
 
-        if (isset($options['data'])) {
+        if (!empty($options['data'])) {
             $form->submit($this->convertData($form, $options['data']));
         } else {
             $request = $this->requestStack->getMasterRequest();
