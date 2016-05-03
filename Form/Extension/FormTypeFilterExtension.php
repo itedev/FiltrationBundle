74b114a70e9f6f31b97700f723b1a23dbddd2323
sam0delkin <t.samodelkin@gmail.com>
2016-05-03 18:56:49 +0600

    fixing filter-form attr

--- Form/Extension/FormTypeFilterExtension.php
+++ Form/Extension/FormTypeFilterExtension.php
@@ -34,8 +34,11 @@ class FormTypeFilterExtension extends AbstractTypeExtension
      */
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
-        $view->vars['filter_form'] = isset($options['filter_form']) ? $options['filter_form'] : false;
-        $view->vars['attr'] = isset($view->vars['attr']) ? array_merge($view->vars['attr'], ['data-filter-form' => true]) : ['data-filter-form' => true];
+        $filterForm = isset($options['filter_form']) ? $options['filter_form'] : false;
+        $view->vars['filter_form'] = $filterForm;
+        if ($filterForm) {
+            $view->vars['attr'] = isset($view->vars['attr']) ? array_merge($view->vars['attr'], ['data-filter-form' => true]) : ['data-filter-form' => true];
+        }
         $view->vars['filter_formatter'] = $options['filter_formatter'];
         $view->vars['filter_formatter_params'] = $options['filter_formatter_params'];
     }
